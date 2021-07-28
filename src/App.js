0549ccc7b2ebd7ca48b106410dec459b3803d1be
Hanalia <davethe@naver.com>
2021-07-28 22:06:51 +0900

    table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,192 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useTable, usePagination, useSortBy } from 'react-table'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import mydata from "./mydata/data.json";
+import styled from "styled-components";
+
+
+const Styles = styled.div`
+
+  .page-link {
+    color: black;
+
+  }
+
+`
+
+
+
+
+function Table({ columns, data }) {
+    // Use the state and functions returned from useTable to build your UI
+    const {
+      getTableProps,
+      getTableBodyProps,
+      headerGroups,
+      prepareRow,
+      page,
+      canPreviousPage,
+      canNextPage,
+      pageOptions,
+      pageCount,
+      gotoPage,
+      nextPage,
+      previousPage,
+      setPageSize,
+      state: { pageIndex, pageSize },
+  } = useTable(
+      {
+          columns,
+          data,
+          initialState: { pageIndex: 2, pageSize: 5 },
+      },
+      useSortBy,
+
+      usePagination
+
+  )
+    // Render the UI for your table
+    return (
+        <div>
+            <table className="table" {...getTableProps()}>
+                <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map(column => (
+                                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                                  {column.render('Header')}
+                                  <span>
+                                        {column.isSorted
+                                            ? column.isSortedDesc
+                                                ? ' 🔽'
+                                                : ' 🔼'
+                                            : ''}
+                                    </span>                         
+                                  
+                                  </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {page.map((row, i) => {
+                        prepareRow(row)
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map(cell => {
+                                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                })}
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+            {/* 
+        Pagination can be built however you'd like. 
+        This is just a very basic UI implementation:
+      */}
+            <ul className="pagination">
+                <li className="page-item" onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                    <a className="page-link">First</a>
+                </li>
+                <li className="page-item" onClick={() => previousPage()} disabled={!canPreviousPage}>
+                    <a className="page-link">{'<'}</a>
+                </li>
+                <li className="page-item" onClick={() => nextPage()} disabled={!canNextPage}>
+                    <a className="page-link">{'>'}</a>
+                </li>
+                <li className="page-item" onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+                    <a className="page-link">Last</a>
+                </li>
+                <li>
+                    <a className="page-link">
+                        Page{' '}
+                        <strong>
+                            {pageIndex + 1} of {pageOptions.length}
+                        </strong>{' '}
+                    </a>
+                </li>
+                <li>
+                    <a className="page-link">
+                        <input
+                            className="form-control"
+                            type="number"
+                            defaultValue={pageIndex + 1}
+                            onChange={e => {
+                                const page = e.target.value ? Number(e.target.value) - 1 : 0
+                                gotoPage(page)
+                            }}
+                            style={{ width: '100px', height: '20px' }}
+                        />
+                    </a>
+                </li>{' '}
+                <select
+                    className="form-control"
+                    value={pageSize}
+                    onChange={e => {
+                        setPageSize(Number(e.target.value))
+                    }}
+                    style={{ width: '120px', height: '38px' }}
+                >
+                    {[5, 10, 20, 30, 40, 50].map(pageSize => (
+                        <option key={pageSize} value={pageSize}>
+                            Show {pageSize}
+                        </option>
+                    ))}
+                </select>
+            </ul>
+        </div >
+    )
 }
 
-export default App;
+function PaginationTableComponent() {
+
+    const columns = React.useMemo(
+  () => [
+   {
+     Header: 'Date',
+     accessor: 'date',
+    //  width: 100,
+   },
+   {
+     Header: 'Title',
+     accessor: 'title',
+     className: "title",
+    //  width: 250,
+     Cell: ({ row }) => <a href={row.original.url}>{row.original.title}</a>,
+   },
+   {
+      Header: 'Title(en)',
+      accessor: 'translated',
+      className: "title",
+      // width: 250,  
+   },
+   {
+     Header: 'Likes',
+     accessor: 'likes',
+    //  width: 60,
+
+   },
+   
+  ],
+  []
+  )
+
+
+       const data = React.useMemo(
+    () => mydata,
+    []
+  )
+
+
+    return (
+      <Styles>
+        <Table columns={columns} data={data} />
+        </Styles>
+    )
+}
+
+export default PaginationTableComponent;
+
+
